159370982ed07df767afa928d1df51c7bc05b63b
David Pádua <david22padua@gmail.com>
2018-12-03 10:42:30 +0000

    Deleted unused import from app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore'; //default export sem {}
-import { addExpense } from './actions/expenses'; //named export com {}
-//import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
