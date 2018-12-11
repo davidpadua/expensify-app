16ac4e00f8fd657a1787daf86d2f5954bf89d7ff
David Pádua <david22padua@gmail.com>
2018-12-11 10:37:37 +0000

    on, off and once methods in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,26 +10,57 @@ const config = {
     messagingSenderId: "768918544317"
   };
 firebase.initializeApp(config);
-
 const database = firebase.database();
 
-database.ref().set({
-      name: 'David Pádua',
-      age: 45,
-      stressLevel: 6,
-      job: {
-          title: 'Front end developer',
-          company: 'DLC'
-      },
-      location: {
-          city: 'Cabo Ruivo',
-          country: 'Portugal'
-      }
-  }).then(() => {
-      console.log('Data is saved!')
-  }).catch((e) => {
-    console.log('This failed.', e);
-  });
+
+database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+})
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//     database.ref('age').set(29);
+// }, 3500)
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000)
+
+// setTimeout(() => {
+//     database.ref('age').set(30);
+// }, 10500)
+// database.ref('location/city')
+//         .once('value')
+//         .then((snapshot) => {
+//             const val = snapshot.val();
+//             console.log(val);
+//         })
+//         .catch(() => {
+//            console.log('Error fetching data', e);
+//         })
+
+// database.ref().set({
+//       name: 'David Pádua',
+//       age: 45,
+//       stressLevel: 6,
+//       job: {
+//           title: 'Front end developer',
+//           company: 'DLC'
+//       },
+//       location: {
+//           city: 'Cabo Ruivo',
+//           country: 'Portugal'
+//       }
+//   }).then(() => {
+//       console.log('Data is saved!')
+//   }).catch((e) => {
+//     console.log('This failed.', e);
+//   });
 
 //:::: REMOVE DATA
 // database.ref()
@@ -42,8 +73,8 @@ database.ref().set({
 //   });
 
 //:::: UPDATE DATA
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Talkdesk',
-    'location/city': 'Saldanha'
-})
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Talkdesk',
+//     'location/city': 'Saldanha'
+// });
