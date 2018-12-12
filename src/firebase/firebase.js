19e456b87d04dc1336653298a3a393ddca7b6f71
David Pádua <david22padua@gmail.com>
2018-12-12 12:13:12 +0000

    Firebase part II

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
 
+
 // Initialize Firebase
 const config = {
     apiKey: "AIzaSyCYpwOagySPcmB3ZzkDt7vcNSCohOF6MBg",
@@ -12,11 +13,94 @@ const config = {
 firebase.initializeApp(config);
 const database = firebase.database();
 
-
-database.ref().on('value', (snapshot) => {
-    const val = snapshot.val();
-    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+// child_removed
+database.ref('expenses').on('child_removed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val())
 })
+
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val())
+})
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
+    console.log(snapshot.key, snapshot.val())
+})
+
+/* database.ref('expenses')
+        .once('value')
+        .then((snapshot) => {
+            const expenses = [];
+
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                   id: childSnapshot.key,
+                   ...childSnapshot.val() 
+                });
+            });
+
+            console.log(expenses);
+        }); */
+/* database.ref('expenses').on('value', (snapshot) => {
+        const expenses = [];
+        snapshot.forEach((childSnapshot) => {
+            expenses.push({
+                id: childSnapshot.key,
+                ...childSnapshot.val() 
+            });
+        });
+
+        console.log(expenses);
+}) */
+database.ref('expenses').push({
+      description: 'Relógio sternglas',
+      amount: 18900,
+      createdAt: 978562695211,
+      note: ''
+})
+
+
+// database.ref('notes/-LTX6hvVcFjOqGAWDkom').update({
+//     body: 'Buy food'
+// })
+//database.ref('notes/-LTX6hvVcFjOqGAWDkom').remove();
+
+// database.ref('notes').push({
+//     title: 'Course Topics',
+//     body: 'React Native, Angular, Python'
+// })
+// const firebaseNotes = {
+//     notes: {
+//         fasdfsdg: {
+//             title: 'First note!',
+//             body: 'This is my note' 
+//         },
+//         dfgsdfhdh: {
+//             title: 'Another note',
+//             body: 'This is my note' 
+//         }
+//     }
+// }
+// const notes = [
+//     {
+//         id: '12',
+//         title: 'First note!',
+//         body: 'This is my note'
+//     },
+//     {
+//         id: '761ase',
+//         title: 'Another note',
+//         body: 'This is my note'
+//     }
+// ]
+// database.ref('notes').set(notes);
+//CRUD >> Creat, Read, Update, Delete
+
+// database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+// })
 // const onValueChange = database.ref().on('value', (snapshot) => {
 //     console.log(snapshot.val());
 // }, (e) => {
