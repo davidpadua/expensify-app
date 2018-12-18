8fcb89d16daac34bb280ce5294c1059de62b0dfb
David Pádua <david22padua@gmail.com>
2018-12-18 10:50:26 +0000

    Setup test database enviroment

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,9 +1,9 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve({
-        //     name: 'David',
-        //     age: 45
-        // });
+        resolve({
+            name: 'David',
+            age: 45
+        });
        reject('Something went wrong');
     }, 1500)
     
@@ -12,9 +12,17 @@ const promise = new Promise((resolve, reject) => {
 console.log('before');
 
 promise.then((data) => {
-    console.log('1', data)
+    console.log('1', data);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('this is my other promise');
+        }, 1500)
+        
+    });
+}).then((str) => {
+    console.log('does this run?', str);
 }).catch((error) => {
-    console.log('error:', error)
+    console.log('error:', error);
 })
 
 
